94ea2f2270411fbefe3a1c14593fc607025a953d
Rob <dev@robrotell.com>
2025-03-16 21:00:21 -0400

    Building get image route

--- src/routes/getImage.js
+++ src/routes/getImage.js
@@ -1,3 +1,5 @@
+import { ImageHandler } from '../clients/ImageHandler.js'
+
 /**
  * Handle requests to get images
  *
@@ -5,6 +7,15 @@
  * @return {void}
  */
 export const getImage = app => {
+	app.get( '/', async c => {
+		const image = await ImageHandler.getRandomImage()
 
+		if( !image ) {
+			return c.json({
+				error: 'No images found.'
+			}, 404 )
+		}
 
+		return c.text( 'here' )
+	})
 }
