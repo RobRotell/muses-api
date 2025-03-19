4f286458e228107facb397a2280e8794b5c5fe62
Rob <dev@robrotell.com>
2025-03-18 23:10:08 -0400

    Building out get entry route

--- src/routes/getEntry.js
+++ src/routes/getEntry.js
@@ -1,3 +1,6 @@
+import { PrismaClient } from '@prisma/client'
+import { PrismaD1 } from '@prisma/adapter-d1'
+
 /**
  * Handle requests to get entries
  *
@@ -7,7 +10,70 @@
  * @return {void}
  */
 export const getEntry = app => {
-	app.get( '/entry', async c => {
-		return c.text( 'bob' )
+	app.get( '/entry/:id?', async c => {
+		let entry
+
+		// prep DB
+		const prisma = new PrismaClient({
+			adapter: new PrismaD1( c.env.DB )
+		})
+
+		let id = c.req.param( 'id' )
+
+		// trying to get specific entry?
+		if( id ) {
+			id = Number.parseInt( id )
+
+			if( Number.isNaN( id ) ) {
+				return c.json({
+					error: 'Entry ID must be an integer.'
+				}, 404 )
+			}
+
+			// try to fetch from DB
+			const row = await prisma.entry.findFirst({
+				where: {
+					id
+				}
+			})
+
+			if( null === row ) {
+				return c.json({
+					error: `ID: ${id} does not match any entry.`
+				}, 404 )
+			}
+
+			entry = row
+
+		// otherwise, fetch random row
+		} else {
+			const rowCount = await prisma.entry.count()
+
+			entry = await prisma.entry.findFirst({
+				skip: Math.floor( rowCount * Math.random() ),
+				take: 1,
+			})
+		}
+
+		// update views count for row
+		await prisma.entry.update({
+			where: {
+				id: entry.id,
+			},
+			data: {
+				// eslint-disable-next-line no-plusplus
+				views: ++entry.views,
+			}
+		})
+
+		return c.json({
+			id: entry.id,
+			prompt: entry.prompt,
+			images: {
+				large: `${c.env.ENDPOINT_URL}/m/${entry.hash}`,
+				medium: `${c.env.ENDPOINT_URL}/m/${entry.hash}/medium`,
+				small: `${c.env.ENDPOINT_URL}/m/${entry.hash}/small`,
+			}
+		})
 	})
 }
