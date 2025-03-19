08d0895bb8d02c2280cffc189ac5f7ebc2e93cc6
Rob <dev@robrotell.com>
2025-03-18 21:50:07 -0400

    DB changes

--- migrations/0002_create_entry_table.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Entry" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "date" DATETIME NOT NULL,
-    "hash" TEXT NOT NULL,
-    "prompt" TEXT NOT NULL,
-    "views" INTEGER NOT NULL
-);
