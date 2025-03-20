c2982c8165cf72b8cb2015fac9916f6628d30354
Rob <dev@robrotell.com>
2025-03-19 21:35:44 -0400

    Finishing up create entry route

--- src/routes/createEntry.js
+++ src/routes/createEntry.js
@@ -1,5 +1,4 @@
 import { GoogleAPI } from '../clients/GoogleAPI'
-import { GoogleAPITemp } from '../clients/GoogleAPITemp'
 import { PrismaClient } from '@prisma/client'
 import { PrismaD1 } from '@prisma/adapter-d1'
 import { getRandomPrompt } from '../utils/prompts'
@@ -18,24 +17,20 @@ export const createEntry = app => {
 
 		// todo -- add validation
 
-		const googleClient = new GoogleAPITemp(
-			c.env.GOOGLE_CLIENT_EMAIL,
-			c.env.GOOGLE_PRIVATE_KEY,
-			c.env.GOOGLE_PROJECT_ID
-		)
+		const googleClient = new GoogleAPI( c.env.GOOGLE_API_KEY )
 
 		const prompt = getRandomPrompt()
 
 		// Google will return a base64 string rep of image
 		let imageBody = await googleClient.generateImage( prompt )
 
-		// convert to something R2 can use
-		imageBody = Uint8Array.from( atob( imageBody ), char => char.charCodeAt( 0 ) )
+		// eslint-disable-next-line no-undef
+		imageBody = Buffer.from( imageBody, 'base64' )
 
 		// hash to uniquely name image
-		const hash = hashValue( prompt )
+		const hash = hashValue( prompt, true )
 
-		// add full/large image to storage
+		// add original image to storage
 		const baseFileName = `${hash}.jpg`
 
 		await c.env.STORAGE.put( baseFileName, imageBody, {
@@ -44,39 +39,31 @@ export const createEntry = app => {
 			}
 		})
 
-		// create and add medium-sized image variant to storage
-		const mediumFileName = `${hash}-medium.jpg`
+		// support variant sizes
+		const variants = {
+			large: 1280,
+			medium: 1024,
+			small: 600,
+		}
 
-		const mediumFileStream = await c.env.IMAGES.input( imageBody ).transform({
-			width: 1024
-		}).output({
-			format: 'image/jpeg'
-		})
+		for( const [
+			size,
+			width
+		] of Object.entries( variants ) ) {
+			const sizedFileStream = await c.env.IMAGES.input( imageBody ).transform({
+				width
+			}).output({
+				format: 'image/jpeg'
+			})
 
-		const mediumFileRes = mediumFileStream.response()
+			const sizedFileRes = sizedFileStream.response()
 
-		await c.env.STORAGE.put( mediumFileName, mediumFileRes.body, {
-			httpMetaData: {
-				contentType: 'image/jpeg'
-			}
-		})
-
-		// now a small image variant (ideal for RaspPi e-ink displays, perhaps?)
-		const smallFileName = `${hash}-small.jpg`
-
-		const smallFileStream = await c.env.IMAGES.input( imageBody ).transform({
-			width: 600
-		}).output({
-			format: 'image/jpeg'
-		})
-
-		const smallFileRes = smallFileStream.response()
-
-		await c.env.STORAGE.put( smallFileName, smallFileRes.body, {
-			httpMetaData: {
-				contentType: 'image/jpeg'
-			}
-		})
+			await c.env.STORAGE.put( `${hash}-${size}.jpg`, sizedFileRes.body, {
+				httpMetaData: {
+					contentType: 'image/jpeg'
+				}
+			})
+		}
 
 		// add entry to DB
 		const prisma = new PrismaClient({
@@ -96,7 +83,8 @@ export const createEntry = app => {
 			id: row.id,
 			prompt: row.prompt,
 			images: {
-				large: `${c.env.ENDPOINT_URL}/m/${row.hash}`,
+				full: `${c.env.ENDPOINT_URL}/m/${row.hash}`,
+				large: `${c.env.ENDPOINT_URL}/m/${row.hash}/large`,
 				medium: `${c.env.ENDPOINT_URL}/m/${row.hash}/medium`,
 				small: `${c.env.ENDPOINT_URL}/m/${row.hash}/small`,
 			}
